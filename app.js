a515d99183dab7d17e83c7d3bb7c302d91c1fbc6
BenQueener <47427613+BenQueener@users.noreply.github.com>
2019-03-13 14:06:57 -0500

    added inputs for start and end year

--- app.js
+++ app.js
@@ -9,8 +9,13 @@ var title ;
 var byline;
 // https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&begin_date=20120101&end_date=20130101&api-key=rcQPGG5PPWkZG0QVBCGythO81oAjiAc2
 
+
+
 $("#search-btn").on("click", function {
     searchTerm = $("#search").val().trim();
+    startYear = $("#start").val().trim();
+    endYear = $("#end").val().trim();
+
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=" + apiKey;
     
     $.ajax({
