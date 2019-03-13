f0d32dc2f3f347eb9f846388f4e266ca53858959
Steve White <stevewhitecodes@gmail.com>
2019-03-13 12:39:24 -0500

    JS file created

---
+++ app.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+var searchTerm;
+var recordsNumber;
+var startYear;
+var endYear;
+
+
+
+
+
+
+
+$("").on("click", function){
+    var queryURL = 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+});
